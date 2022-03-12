52a00b4b6ceca30c0e7ab40cb0922cb29f7820b2
Sergi Mayol <sergi2808@gmail.com>
2022-03-12 17:15:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from openpyxl import load_workbook
 from openpyxl.utils import get_column_letter
 import re
 
-wb = load_workbook("p.xlsx")
+tmp = str(input("Path excel: "))
+wb = load_workbook(tmp)
 ws = wb.active
-file = open("C:/Users/Sergi/Desktop/prueba.txt")
+tmp = input("Path fichero de texto: ")
+file = open(tmp)
 
 if __name__ == '__main__':
     text = file.read(1)
@@ -12,36 +14,39 @@ if __name__ == '__main__':
     row = 2
     col = 1
     while True:
-        # Check EOF
-        if not text:
-            break
-        # Add value
-        if text == ":":
-            ws[get_column_letter(col)+str(row)].value = info + ":"
+        try:
+            # Check EOF
+            if not text:
+                break
+            # Add value
+            if text == ":":
+                ws[get_column_letter(col)+str(row)].value = info + ":"
+                text = file.read(1)
+                col = col + 1
+                info = ""
+
+            if text == ",":
+                # Quitar a info los valores no númericos
+                aux = [float(s) for s in re.findall(r'-?\d+\.?\d*', info)]
+                ws[get_column_letter(col) + str(row)].value = aux[0]
+                text = file.read(1)
+                col = col + 1
+                info = ""
+
+            if text == "\n":
+                # Quitar a info los valores no númericos
+                aux = [float(s) for s in re.findall(r'-?\d+\.?\d*', info)]
+                ws[get_column_letter(col) + str(row)].value = aux[0]
+                # siguiente linea y reset columna e info
+                row = row + 1
+                col = 1
+                info = ""
+
+            # Add en info solo nums.
+            info += text
             text = file.read(1)
-            col = col + 1
-            info = ""
-
-        if text == ",":
-            # Quitar a info los valores no númericos
-            aux = [float(s) for s in re.findall(r'-?\d+\.?\d*', info)]
-            ws[get_column_letter(col) + str(row)].value = aux[0]
-            text = file.read(1)
-            col = col + 1
-            info = ""
-
-        if text == "\n":
-            # Quitar a info los valores no númericos
-            aux = [float(s) for s in re.findall(r'-?\d+\.?\d*', info)]
-            ws[get_column_letter(col) + str(row)].value = aux[0]
-            # siguiente linea y reset columna e info
-            row = row + 1
-            col = 1
-            info = ""
-
-        # Add en info solo nums.
-        info += text
-        text = file.read(1)
+        except Exception as e:
+            print(e)
 
     # End of while, close buffer and save info excel
     file.close()
