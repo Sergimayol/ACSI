ae568f8fae75a9556b7ad9c112d67cb02697ecfd
Sergi Mayol <sergi2808@gmail.com>
2022-03-12 16:40:30 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+from openpyxl import load_workbook
+from openpyxl.utils import get_column_letter
+import re
+
+wb = load_workbook("p.xlsx")
+ws = wb.active
+file = open("C:/Users/Sergi/Desktop/prueba.txt")
+
+if __name__ == '__main__':
+    text = file.read(1)
+    info = ""
+    row = 2
+    col = 1
+    while True:
+        # Check EOF
+        if not text:
+            break
+        # Add value
+        if text == ":":
+            ws[get_column_letter(col)+str(row)].value = info + ":"
+            text = file.read(1)
+            col = col + 1
+            info = ""
+
+        if text == ",":
+            # Quitar a info los valores no númericos
+            aux = [float(s) for s in re.findall(r'-?\d+\.?\d*', info)]
+            ws[get_column_letter(col) + str(row)].value = aux[0]
+            text = file.read(1)
+            col = col + 1
+            info = ""
+
+        if text == "\n":
+            # Quitar a info los valores no númericos
+            aux = [float(s) for s in re.findall(r'-?\d+\.?\d*', info)]
+            ws[get_column_letter(col) + str(row)].value = aux[0]
+            # siguiente linea y reset columna e info
+            row = row + 1
+            col = 1
+            info = ""
+
+        # Add en info solo nums.
+        info += text
+        text = file.read(1)
+
+    # End of while, close buffer and save info excel
+    file.close()
+    wb.save("p.xlsx")
