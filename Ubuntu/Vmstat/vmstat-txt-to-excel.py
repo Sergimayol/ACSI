e5e7be7ff091d5136eb6657fb8123149d6370aba
Sergi Mayol <sergi2808@gmail.com>
2022-03-18 15:59:03 +0100

    Update vmstat-txt-to-excel.py

--- Ubuntu/Vmstat/vmstat-txt-to-excel.py
+++ Ubuntu/Vmstat/vmstat-txt-to-excel.py
@@ -11,4 +11,30 @@ ws = wb.active
 if __name__ == "__main__":
     # 2 primeras lineas son nombres cols
     text = file.readline(2)
-    # aqui ya viene la info
+    info = ""
+    row = 2
+    col = 1
+    while True:
+        if not text:
+            break
+        if text == " ":
+            ws[get_column_letter(col) + str(row)].value = info + ":"
+            text = file.read(1)
+            col = col + 1
+            info = ""
+
+        if text == "\n":
+            ws[get_column_letter(col) + str(row)].value = info
+            # siguiente linea y reset columna e info
+            row = row + 1
+            col = 1
+            info = ""
+
+        info += text
+        text = file.read(1)
+
+    # End of while, close buffer and save info excel
+    file.close()
+    wb.save(tmp)
+    print("Proceso finalizado con éxito :D")
+    input("<<<<<<Pulse cualquier tecla para finalizar>>>>>>")
