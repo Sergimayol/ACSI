fac3cd5c779e93c9843bb970f8cf589b17e6caac
Sergi Mayol <sergi2808@gmail.com>
2022-04-11 21:58:59 +0200

    Servidor a y b obtenidos + apartado 1.2

---
+++ CP_p3/scriptWsl.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#cd /mnt/c/Users/Sergi/Desktop
+#cd /media/sf_Compartido/Pract3
+echo "Setup Servidores A y B"
+echo "Servidor A:"
+time sysbench cpu --cpu-max-prime=250000 --threads=2 run >> ServidorA.txt
+time sysbench cpu --cpu-max-prime=250000 --threads=2 run >> ServidorA.txt
+time sysbench cpu --cpu-max-prime=250000 --threads=2 run >> ServidorA.txt
+time sysbench cpu --cpu-max-prime=250000 --threads=2 run >> ServidorA.txt
+time sysbench cpu --cpu-max-prime=250000 --threads=2 run >> ServidorA.txt
+time sysbench cpu --cpu-max-prime=250000 --threads=2 run >> ServidorA.txt
+echo "---------------------------------"
+echo "Servidor B:"
+time sysbench cpu --cpu-max-prime=300000 --threads=4 run >> ServidorB.txt
+time sysbench cpu --cpu-max-prime=300000 --threads=4 run >> ServidorB.txt
+time sysbench cpu --cpu-max-prime=300000 --threads=4 run >> ServidorB.txt
+time sysbench cpu --cpu-max-prime=300000 --threads=4 run >> ServidorB.txt
+time sysbench cpu --cpu-max-prime=300000 --threads=4 run >> ServidorB.txt
+time sysbench cpu --cpu-max-prime=300000 --threads=4 run >> ServidorB.txt
+time sysbench cpu --cpu-max-prime=300000 --threads=4 run >> ServidorB.txt
+echo "---------------------------------"
+echo "Apartado 1.2"
+echo "BENCHMARK: Sysbench carga = 25000"
+time sysbench cpu --cpu-max-prime=25000 --threads=2 run > sysbench25k.txt
+echo "---------------------------------"
+echo "BENCHMARK: Sysbench carga = 50000"
+time sysbench cpu --cpu-max-prime=50000 --threads=2 run > sysbench50k.txt
+echo "---------------------------------"
+echo "BENCHMARK: Sysbench carga = 100000"
+time sysbench cpu --cpu-max-prime=100000 --threads=2 run > sysbench100k.txt
+echo "---------------------------------"
+echo "BENCHMARK: Sysbench carga = 150000"
+time sysbench cpu --cpu-max-prime=150000 --threads=2 run > sysbench150k.txt
+echo "---------------------------------"
