7f3b50b16f77f5481865a70b908876deab71fffb
Sergi Mayol <sergi2808@gmail.com>
2022-04-12 19:21:38 +0200

    Ficheros con datos, todos ya hechos

--- CP_p3/scriptWsl.sh
+++ CP_p3/scriptWsl.sh
@@ -2,10 +2,10 @@
 echo "Setup Servidores A y B"
 echo "Servidor A:"
 for i in $(seq 1 10); do
-    time sysbench cpu --cpu-max-prime=250000 --threads=8 run >>ServidorA.txt
+    time sysbench cpu --cpu-max-prime=250000 --threads=6 run >>ServidorA.txt
 done
 echo "---------------------------------"
 echo "Servidor B:"
 for i in $(seq 1 10); do
-    time stress-ng --cpu 8 --cpu-ops=300000
+    time stress-ng --cpu 12 --cpu-ops=300000
 done
