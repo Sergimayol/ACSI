8df21d186e605253722199207c3406345a6551f7
Sergi Mayol <sergi2808@gmail.com>
2022-04-12 09:54:37 +0200

    Update scriptWsl.sh

--- CP_p3/scriptWsl.sh
+++ CP_p3/scriptWsl.sh
@@ -4,32 +4,11 @@ echo "Servidor A:"
 for i in $(seq 1 10); do
     time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
 done
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
-time sysbench cpu --cpu-max-prime=250000 --threads=2 run >>ServidorA.txt
 echo "---------------------------------"
 echo "Servidor B:"
 for i in $(seq 1 10); do
-    time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
+    time stress-ng --cpu 4 --cpu-ops=300000 --log-file ServidorB.txt
 done
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
-time sysbench cpu --cpu-max-prime=300000 --threads=4 run >>ServidorB.txt
 echo "---------------------------------"
 echo "Apartado 1.2"
 echo "BENCHMARK: Sysbench carga = 25000"
