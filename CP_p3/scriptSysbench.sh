bd49caca1bfd09fcbb51f75850440a54cccf3c21
Sergi Mayol <sergi2808@gmail.com>
2022-04-14 19:04:14 +0200

    files changes

--- CP_p3/scriptSysbench.sh
+++ CP_p3/scriptSysbench.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
+read res
 echo "---------------------------------"
 echo "Apartado 1.2"
 echo "BENCHMARK: Sysbench carga = 25000"
 for i in $(seq 1 10); do
     time sysbench cpu --cpu-max-prime=25000 --threads=6 run >>sysbench25k.txt
 done
+read res
 echo "---------------------------------"
 echo "BENCHMARK: Sysbench carga = 50000"
 for i in $(seq 1 10); do
     time sysbench cpu --cpu-max-prime=50000 --threads=6 run >>sysbench50k.txt
 done
+read res
 echo "---------------------------------"
 echo "BENCHMARK: Sysbench carga = 100000"
 for i in $(seq 1 10); do
     time sysbench cpu --cpu-max-prime=100000 --threads=6 run >>sysbench100k.txt
 done
+read res
 echo "---------------------------------"
 echo "BENCHMARK: Sysbench carga = 150000"
 for i in $(seq 1 10); do
