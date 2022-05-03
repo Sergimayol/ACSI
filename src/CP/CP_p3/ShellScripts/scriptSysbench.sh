40a3c227e967e4017b4205e6ab9c48096c12b4e6
Sergi Mayol <sergi2808@gmail.com>
2022-05-03 08:54:25 +0200

    .sh updated

--- src/CP/CP_p3/ShellScripts/scriptSysbench.sh
+++ src/CP/CP_p3/ShellScripts/scriptSysbench.sh
@@ -4,24 +4,28 @@ echo "---------------------------------"
 echo "Apartado 1.2"
 echo "BENCHMARK: Sysbench carga = 25000"
 for i in $(seq 1 10); do
-    time sysbench cpu --cpu-max-prime=25000 --threads=6 run >>sysbench25k.txt
+    #time sysbench cpu --cpu-max-prime=25000 --threads=6 run >>sysbench25k.txt
+    time sysbench cpu --cpu-max-prime=25000 --time=0 --threads=6 --max-requests=25000 run >>sysbench25k.txt
 done
 read res
 echo "---------------------------------"
 echo "BENCHMARK: Sysbench carga = 50000"
 for i in $(seq 1 10); do
-    time sysbench cpu --cpu-max-prime=50000 --threads=6 run >>sysbench50k.txt
+    #time sysbench cpu --cpu-max-prime=50000 --threads=6 run >>sysbench50k.txt
+    time sysbench cpu --cpu-max-prime=50000 --time=0 --threads=6 --max-requests=50000 run >>sysbench25k.txt
 done
 read res
 echo "---------------------------------"
 echo "BENCHMARK: Sysbench carga = 100000"
 for i in $(seq 1 10); do
-    time sysbench cpu --cpu-max-prime=100000 --threads=6 run >>sysbench100k.txt
+    #time sysbench cpu --cpu-max-prime=100000 --threads=6 run >>sysbench100k.txt
+    time sysbench cpu --cpu-max-prime=100000 --time=0 --threads=6 --max-requests=100000 run >>sysbench25k.txt
 done
 read res
 echo "---------------------------------"
 echo "BENCHMARK: Sysbench carga = 150000"
 for i in $(seq 1 10); do
-    time sysbench cpu --cpu-max-prime=150000 --threads=6 run >>sysbench150k.txt
+    #time sysbench cpu --cpu-max-prime=150000 --threads=6 run >>sysbench150k.txt
+    time sysbench cpu --cpu-max-prime=150000 --time=0 --threads=6 --max-requests=150000 run >>sysbench25k.txt
 done
 echo "---------------------------------"
--- src/CP/CP_p3/ShellScripts/scriptWsl.sh
+++ src/CP/CP_p3/ShellScripts/scriptWsl.sh
@@ -2,7 +2,8 @@
 echo "Setup Servidores A y B"
 echo "Servidor A:"
 for i in $(seq 1 10); do
-    time sysbench cpu --cpu-max-prime=250000 --threads=6 run >>ServidorA.txt
+    #time sysbench cpu --cpu-max-prime=250000 --threads=6 run >>ServidorA.txt
+    time sysbench cpu --cpu-max-prime=250000 --time=0 --threads=6 --max-requests=250000 run >>ServidorA.txt
 done
 echo "---------------------------------"
 echo "Servidor B:"
