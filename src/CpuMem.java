d82738d0f7fc10e83fe93a681392c668b212ef13
Sergi Mayol <sergi2808@gmail.com>
2022-05-07 12:33:56 +0200

    pract3 :white_check_mark:

---
+++ src/CpuMem.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package borrador;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sergi
+ */
+public class CpuMem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(System.in);
+        ArrayList<String> cpu = new ArrayList();
+        ArrayList<String> mem = new ArrayList();
+        String aux;
+        for (int i = 0; i < 12; i++) {
+            aux = s.nextLine();
+            cpu.add(aux);
+            aux = s.nextLine();
+            mem.add(aux);
+        }
+        System.out.println("\n\n\n");
+        for (int i = 0; i < cpu.size(); i++) {
+            System.out.println(cpu.get(i));
+        }
+        for (int i = 0; i < mem.size(); i++) {
+            System.out.println(mem.get(i));
+        }
+    }
+
+}
