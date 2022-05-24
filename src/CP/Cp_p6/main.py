8de08ad8b01f5a8e7be7224ef0734f31b2954c06
Sergi Mayol <sergi2808@gmail.com>
2022-05-24 12:25:12 +0200

    Create main.py

---
+++ src/CP/Cp_p6/main.py
@@ -0,0 +1,22 @@
+def analyze(line):
+    try:
+        if '-1' in line:
+            # Not write line
+            pass
+    except ValueError as e:
+        print("Error: {}".format(e))
+
+
+if __name__ == '__main__':
+    line = ""
+    file = open(str(input("Path fichero: ")))
+    while True:
+        try:
+            line = file.readline()
+            # EOF
+            if not line:
+                break
+            # Analyze sentence
+            analyze(line)
+        except Exception as e:
+            print("Error: {}".format(e))
