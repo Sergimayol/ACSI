ed9cdf0a180d8d8e27167fed45cd5c217e331cd2
Sergi Mayol <sergi2808@gmail.com>
2022-05-26 19:19:46 +0200

    update

--- src/CP/CP_p5/MVA.java
+++ src/CP/CP_p5/MVA.java
@@ -10,23 +10,59 @@ import java.util.List;
  */
 public class MVA {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        try {
+            new MVA().test1();
+            new MVA().test2();
+            new MVA().test3();
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
 
+    private void test1() {
+        System.out.println("--------- TEST 1 ---------");
+        int N = 3 + 1;
+        int Z = 5;
+        int K = 2;
+        double[] Si = { 0.03, 0.5 };
+        int[] Vi = { 15, 14 };
+        MeanValueAnalysis(N, Z, K, Si, Vi);
+    }
+
+    private void test2() {
+        System.out.println("\n\n\n--------- TEST 2 ---------");
+        int N = 10 + 1;
+        int Z = 5;
+        int K = 2;
+        double[] Si = { 0.03, 0.1 };
+        int[] Vi = { 8, 7 };
+        MeanValueAnalysis(N, Z, K, Si, Vi);
+    }
+
+    private void test3() {
+        System.out.println("\n\n\n--------- TEST 3 ---------");
+        int N = 5 + 1;
+        int Z = 2;
+        int K = 2;
+        double[] Si = { 0.1, 0.2 };
+        int[] Vi = { 8, 7 };
+        MeanValueAnalysis(N, Z, K, Si, Vi);
     }
 
     public static void MeanValueAnalysis(int N, int Z, int K, double[] Si, int[] Vi) {
         Double[] Ni = new Double[K];
         Double[] Ri = new Double[K];
         Double[] Xi = new Double[K];
+        Double[] Ui = new Double[K];
         double X0 = 0.0;
         double[] Di = new double[K];
+        double D = 0;
         List<Double> _list_X0 = new ArrayList<Double>();
         List<Double> _list_R = new ArrayList<Double>();
         for (int i = 0; i < K; i++) {
             Di[i] = Si[i] * Vi[i];
+            D += Di[i];
         }
         for (int i = 0; i < K; i++) {
             Ni[i] = 0.0;
@@ -45,15 +81,19 @@ public class MVA {
             _list_X0.add(X0);
             for (int i = 0; i < K; i++) {
                 Ni[i] = (X0 * Vi[i] * Ri[i]);
+                Ui[i] = (X0 * Vi[i] * Si[i]);
                 Xi[i] = (X0 * Vi[i]);
             }
-            System.out.println("Xi: " + Arrays.toString(Xi));
-            System.out.println("Ni: " + Arrays.toString(Ni));
-            System.out.println("Ri: " + Arrays.toString(Ri));
-            System.out.println("X0: " + X0);
-            System.out.println("_list_R: " + _list_R);
-            System.out.println("_list_X0: " + _list_X0);
-            System.out.println("R: " + R);
+            if (n != 0) {
+                System.out.println("Trabajo: " + n);
+                System.out.println("Xi: " + Arrays.toString(Xi));
+                System.out.println("Ni: " + Arrays.toString(Ni));
+                System.out.println("Ri: " + Arrays.toString(Ri));
+                System.out.println("Ui: " + Arrays.toString(Ui));
+                System.out.println("X0: " + X0);
+                System.out.println("R: " + R);
+                System.out.println("");
+            }
         }
     }
 }
--- src/CP/CP_p5/main.py
+++ src/CP/CP_p5/main.py
@@ -1,29 +1,101 @@
-from openpyxl import load_workbook
-from openpyxl.utils import get_column_letter
-import re
+# from openpyxl import load_workbook
+# from openpyxl.utils import get_column_letter
+# import re
 
 
-def line_to_excel(line):
-    line_with_number = ""
-    try:
-        # get numbers from the line
-        for i in range(len(line)):
-            if line[i].isdigit():
-                line_with_number += str(line[i])
-    except ValueError as e:
-        print("Error: {}".format(e))
+# def line_to_excel(line):
+#     line_with_number = ""
+#     try:
+
+#     except ValueError as e:
+#         print("Error: {}".format(e))
 
 
 def main():
     file = open("input.txt", "r")
-    line = file.readline()
+    line = ""
+    res = ""
     while True:
         try:
+            line = file.read(5)
             # Check EOF
             if not line:
                 break
             # Check if line is valid
-            line_to_excel(line)
+            # line_to_excel(line)
+            while True:
+                line = file.read(1)
+                if line == ",":
+                    file.read(1)
+                    break
+                res += line
+            print("X1: {}".format(res))
+            res = ""
+            while True:
+                line = file.read(1)
+                if line == "]":
+                    break
+                res += line
+            print("X2: {}".format(res))
+            res = ""
+            # Next line
+            file.read(1)
+            line = file.read(5)
+            while True:
+                line = file.read(1)
+                if line == ",":
+                    file.read(1)
+                    break
+                res += line
+            print("N1: {}".format(res))
+            res = ""
+            while True:
+                line = file.read(1)
+                if line == "]":
+                    break
+                res += line
+            print("N2: {}".format(res))
+            res = ""
+            # Next line
+            file.read(1)
+            line = file.read(5)
+            while True:
+                line = file.read(1)
+                if line == ",":
+                    file.read(1)
+                    break
+                res += line
+            print("R1: {}".format(res))
+            res = ""
+            while True:
+                line = file.read(1)
+                if line == "]":
+                    break
+                res += line
+            print("R2: {}".format(res))
+            res = ""
+            # Next line
+            file.read(1)
+            line = file.read(5)
+            while True:
+                line = file.read(1)
+                if line == ",":
+                    file.read(1)
+                    break
+                res += line
+            print("U1: {}".format(res))
+            res = ""
+            while True:
+                line = file.read(1)
+                if line == "]":
+                    break
+                res += line
+            print("U2: {}".format(res))
+            res = ""
+            # Next line
+            file.read(1)
+            file.readline()
+            file.readline()
         except Exception as e:
             print("Error: {}".format(e))
 
